f9690fd90b067d29ac75de16e2a898352a137449
Ken Sedgwick <ken@bonsai.com>
2019-05-25 20:49:04 -0700

    Added check routines for route fee and delay computations.

--- rebalance.go
+++ rebalance.go
@@ -13,15 +13,98 @@ import (
     "github.com/lightningnetwork/lnd/lnrpc"
 )
 
-func hopPolicy(client lnrpc.LightningClient, ctx context.Context, chanId uint64, dstNode string) *lnrpc.RoutingPolicy {
+func hopPolicy(client lnrpc.LightningClient, ctx context.Context, chanId uint64, dstNode string) (*lnrpc.RoutingPolicy, *lnrpc.RoutingPolicy) {
 	chanInfo, err := client.GetChanInfo(ctx, &lnrpc.ChanInfoRequest{ChanId: chanId})
 	if err != nil {
 		panic(fmt.Sprintf("last GetChanInfo failed:", err))
 	}
 	if chanInfo.Node1Pub == dstNode {
-		return chanInfo.Node2Policy
+		return chanInfo.Node2Policy, chanInfo.Node1Policy
 	} else {
-		return chanInfo.Node1Policy
+		return chanInfo.Node1Policy, chanInfo.Node2Policy
+	}
+}
+
+func hopStr(info *lnrpc.GetInfoResponse, hop *lnrpc.Hop) string {
+	return fmt.Sprintf("%d %10d %7d %10d %4d %7d %4d %s",
+		hop.ChanId,
+		hop.ChanCapacity,
+		hop.AmtToForward,
+		hop.AmtToForwardMsat,
+		hop.Fee,
+		hop.FeeMsat,
+		hop.Expiry - info.BlockHeight,
+		hop.PubKey,
+	)
+}
+
+func policyStr(info *lnrpc.GetInfoResponse, sndPolicy *lnrpc.RoutingPolicy, rcvPolicy *lnrpc.RoutingPolicy) string {
+	return fmt.Sprintf("%7d %4d %4d",
+		sndPolicy.FeeBaseMsat,
+		sndPolicy.FeeRateMilliMsat,
+		sndPolicy.TimeLockDelta,
+		// rcvPolicy.FeeBaseMsat,
+		// rcvPolicy.FeeRateMilliMsat,
+		// rcvPolicy.TimeLockDelta,
+	)
+}
+
+func routeStr(info *lnrpc.GetInfoResponse, route *lnrpc.Route) string {
+	return fmt.Sprintf("%29s %7d %10d %4d %7d %4d",
+		"",
+		route.TotalAmt,
+		route.TotalAmtMsat,
+		route.TotalFees,
+		route.TotalFeesMsat,
+		route.TotalTimeLock - info.BlockHeight,
+	)
+}
+
+func checkRoute(client lnrpc.LightningClient, ctx context.Context, info *lnrpc.GetInfoResponse, route *lnrpc.Route) {
+	ll := len(route.Hops)
+	
+	sumDelta := uint32(9)
+	lastDelta := uint32(0)
+	
+	sumFeeMsat := int64(0)
+	lastFeeMsat := int64(0)
+	
+	for ndx := ll - 1; ndx >= 0; ndx-- {
+		hop := route.Hops[ndx]
+		sndPolicy, _ := hopPolicy(client, ctx, hop.ChanId, hop.PubKey)
+
+		if ndx == ll - 1 {
+			// Last hop
+			if hop.Expiry - info.BlockHeight != sumDelta {
+				panic(fmt.Sprintf("bad expiry on hop %d", ndx))
+			}
+		} else if ndx == ll - 2 {
+			// Second to last hop
+			if hop.Expiry - info.BlockHeight != sumDelta {
+				panic(fmt.Sprintf("bad expiry on hop %d", ndx))
+			}
+			sumDelta += lastDelta
+		} else {
+			// Prior hops
+			if hop.Expiry - info.BlockHeight != sumDelta {
+				panic(fmt.Sprintf("bad expiry on hop %d", ndx))
+			}
+			sumDelta += lastDelta
+		}
+		lastDelta = sndPolicy.TimeLockDelta
+
+		if hop.FeeMsat != lastFeeMsat {
+			panic(fmt.Sprintf("bad fee on hop %d, saw %d, expected %d", ndx, lastFeeMsat, hop.FeeMsat))
+		}
+		sumFeeMsat += lastFeeMsat
+		
+		lastFeeMsat = sndPolicy.FeeBaseMsat + (hop.AmtToForwardMsat * sndPolicy.FeeRateMilliMsat) / 1000000
+	}
+	if route.TotalTimeLock - info.BlockHeight != sumDelta {
+		panic(fmt.Sprintf("bad route total"))
+	}
+	if route.TotalFeesMsat != sumFeeMsat {
+		panic(fmt.Sprintf("bad fee total"))
 	}
 }
 
@@ -89,7 +172,7 @@ func rebalance(client lnrpc.LightningClient, ctx context.Context, args []string)
 				Fixed: feeLimitFixed,
 			},
 		},
-		SourcePubKey: srcPubKey,
+		// SourcePubKey: srcPubKey,
 	})
     if err != nil {
 		panic(fmt.Sprintf("QueryRoutes failed:", err))
@@ -99,17 +182,20 @@ func rebalance(client lnrpc.LightningClient, ctx context.Context, args []string)
 	for _, route := range rsp.Routes {
 		fmt.Println()
 		for ndx, hop := range route.Hops {
-			// Is this the last hop in the partial route?
-			policy := hopPolicy(client, ctx, hop.ChanId, hop.PubKey)
-			if ndx == len(route.Hops) - 1 {
-				// Add in the fees for the last hop (since we are adding another)
-				hop.Fee = (policy.FeeBaseMsat +
-					(policy.FeeRateMilliMsat * int64(amt) / 1000)) / 1000
-				route.TotalFees += hop.Fee
+
+			sndPolicy, rcvPolicy :=
+				hopPolicy(client, ctx, hop.ChanId, hop.PubKey)
+
+			pstr := ""
+			if ndx != 0 {
+				pstr = policyStr(info, sndPolicy, rcvPolicy)
 			}
-			fmt.Printf("%d %s %6d %5d\n", hop.ChanId, hop.PubKey, hop.Fee, policy.TimeLockDelta)
+			
+			fmt.Printf("%s %s\n", hopStr(info, hop), pstr)
 		}
-		fmt.Printf("                                                                                      %6d %5d\n", route.TotalFees, route.TotalTimeLock - info.BlockHeight)
+		fmt.Printf("%s\n", routeStr(info, route))
+
+		checkRoute(client, ctx, info, route)
 	}
 
 	os.Exit(0)
